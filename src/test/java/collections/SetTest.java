66116e7bf3479ae2d18a3941e6f39428602202ed
MaxDoroshenko <m.doroshenko24@gmail.com>
2020-11-21 19:42:50 +0200

    Коллеккции

---
+++ src/test/java/collections/SetTest.java
@@ -0,0 +1,16 @@
+package collections;
+import java.util.*;
+
+public class SetTest {
+    public static void main(String[] args) {
+
+        HashSet<Integer> myHashSet = new HashSet<Integer>();
+
+        myHashSet.add(1);
+        myHashSet.add(1);
+
+        for (int i : myHashSet)
+            System.out.println(i);
+
+    }
+}
