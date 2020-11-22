a5cd29d839dfccb4e5a686593c22a00dc0c8b983
MaxDoroshenko <m.doroshenko24@gmail.com>
2020-11-22 14:01:57 +0200

    Коллеккции

--- src/test/java/collections/HashMapTest.java
+++ src/test/java/collections/HashMapTest.java
@@ -4,12 +4,12 @@ public class HashMapTest {
 
         public static void main(String args[]){
             HashMap<Integer,String> map = new HashMap<>();
-            map.put(1,"Mango");  //Put elements in Map
-            map.put(2,"Apple");
-            map.put(3,"Banana");
-            map.put(4,"Grapes");
+            map.put(1,"Один");  //Put elements in Map
+            map.put(2,"Два");
+            map.put(3,"Три");
+            map.put(4,"Четыре");
 
-            System.out.println("Iterating Hashmap...");
+            System.out.println("Hashmap:");
             for(Map.Entry m : map.entrySet()){
                 System.out.println(m.getKey()+" "+m.getValue());
             }
--- src/test/java/collections/SetTest.java
+++ src/test/java/collections/SetTest.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class SetTest {
     public static void main(String[] args) {
 
-        HashSet<Integer> myHashSet = new HashSet<Integer>();
+        HashSet<Integer> myHashSet = new HashSet<>();
 
         myHashSet.add(1);
         myHashSet.add(1);
