d8767d86902f0fdfd3882a7100f109a3f52e2c9f
MaxDoroshenko <m.doroshenko24@gmail.com>
2020-11-22 14:06:49 +0200

    Коллеккции

--- src/test/java/collections/HashMapTest.java
+++ src/test/java/collections/HashMapTest.java
@@ -4,14 +4,14 @@ public class HashMapTest {
 
         public static void main(String args[]){
             HashMap<Integer,String> map = new HashMap<>();
-            map.put(1,"Один");  //Put elements in Map
+            map.put(1,"Один");
             map.put(2,"Два");
             map.put(3,"Три");
             map.put(4,"Четыре");
 
             System.out.println("Hashmap:");
             for(Map.Entry m : map.entrySet()){
-                System.out.println(m.getKey()+" "+m.getValue());
+                System.out.println(m.getKey() + " " + m.getValue());
             }
         }
 }
