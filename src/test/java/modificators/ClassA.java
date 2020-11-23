31c7a16823eaa1fde19904c41b82b9e724ebfd9d
MaxDoroshenko <m.doroshenko24@gmail.com>
2020-11-23 22:44:06 +0200

    Модификаторы (исправление)

--- src/test/java/modificators/ClassA.java
+++ src/test/java/modificators/ClassA.java
@@ -2,6 +2,6 @@ package modificators;
 
 public class ClassA {
     public void publicMethod(){System.out.println("Public method from class A");}
-    private void privateMethod(){System.out.println("Private method from class A");}
+    void packagePrivateMethod(){System.out.println("Package private method from class A");}
     protected void protectedMethod(){System.out.println("Protected method from class A");}
 }
--- src/test/java/modificators/ClassC.java
+++ src/test/java/modificators/ClassC.java
@@ -4,11 +4,8 @@ public class ClassC {
     public static void main(String[] args) {
         ClassA A = new ClassA();
         A.publicMethod();
-
-        //Непонятно задание с вызовом метода private, поэтому создал еще один метод и вызвал в классе, иначе
-        //код A.privateMethod(); приводит к ошибке
-        privateMethodC();
+        A.packagePrivateMethod();
     }
-    private static void privateMethodC(){System.out.println("Private method from class C");}
+
     public static void publicMethodC(){System.out.println("Public static method from class C");}
 }
