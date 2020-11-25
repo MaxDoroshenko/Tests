6aa7d5fabd3298fa2f14010b95a0916685b1f951
MaxDoroshenko <m.doroshenko24@gmail.com>
2020-11-25 03:23:57 +0200

    Коллекции (альтернативная реализация)

--- src/test/java/collections/HashMapTest.java
+++ src/test/java/collections/HashMapTest.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class HashMapTest {
 
         public static void main(String args[]){
-            HashMap<Integer,String> map = new HashMap<>();
+            Map<Integer,String> map = new HashMap<>();
             map.put(1,"Один");
             map.put(2,"Два");
             map.put(3,"Три");
@@ -13,5 +13,17 @@ public class HashMapTest {
             for(Map.Entry m : map.entrySet()){
                 System.out.println(m.getKey() + " " + m.getValue());
             }
+
+            //Другой вариант реализации
+            /*HashMap map = new HashMap();
+            map.put("1","Один");
+            map.put(2,"Два");
+            map.put("Three", "Три");
+            map.put(4,"Четыре");
+            map.put("qwerty", 12345);
+            System.out.println("HashMap:");
+            for(Object m : map.entrySet() ){
+                System.out.println(m);
+            }*/
         }
 }
--- src/test/java/collections/SetTest.java
+++ src/test/java/collections/SetTest.java
@@ -4,13 +4,23 @@ import java.util.*;
 public class SetTest {
     public static void main(String[] args) {
 
-        HashSet<Integer> myHashSet = new HashSet<>();
+        Set<Integer> myHashSet = new HashSet<>();
 
         myHashSet.add(1);
         myHashSet.add(1);
 
         for (int i : myHashSet)
             System.out.println(i);
+
+        //-----------Другой вариант реализации
+        /*Collection myHashSet2 = new HashSet();
+        //Set myHashSet2 = new HashSet();
+        myHashSet2.add(1);
+        myHashSet2.add(1);
+
+        for(Object i : myHashSet2)
+            System.out.println(i);*/
+
 
     }
 }
