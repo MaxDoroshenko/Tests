b766c87a3d62f019f28db0fcaeb1a818f234a827
MaxDoroshenko <m.doroshenko24@gmail.com>
2021-01-05 02:49:33 +0200

    ToJSON
    (cherry picked from commit ca18fba66884aef2d87c5f8d9616a9e96d29da5b)

---
+++ src/test/java/json/ToJson.java
@@ -0,0 +1,14 @@
+package json;
+
+import javax.json.Json;
+
+import javax.json.JsonObject;
+
+public class ToJson {
+    public static void main(String[] args) {
+        JsonObject jo = Json.createObjectBuilder()
+                .add("a", "aaa")
+                .add("b", "bbb")
+                .build();
+    }
+}
